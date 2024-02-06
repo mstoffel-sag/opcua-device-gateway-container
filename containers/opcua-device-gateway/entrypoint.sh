5c22ba5e78c51539dba23ad642b8e2aa4a610d46
Reuben Miller <reuben.d.miller@gmail.com>
2024-02-06 09:51:33 +0100

    feat: prefix opcua identifier with the thin-edge.io device.id

--- containers/opcua-device-gateway/entrypoint.sh
+++ containers/opcua-device-gateway/entrypoint.sh
@@ -14,8 +14,24 @@ if command -V tedge >/dev/null 2>&1; then
         echo "Using value from tedge: C8Y_BASEURL=$C8Y_BASEURL" >&2
         export C8Y_BASEURL
     fi
+
+    # Add a prefix to the OPCUA identifier to ensure its external identity is unique
+    # to allow deploying multiple versions in the fleet without having to define unique names all the time
+    case "$OPCUA_GATEWAY_IDENTIFIER" in
+        *"$DEVICE_ID"*)
+            echo "Gateway identifier already includes the device_id" >&2
+            ;;
+        *)
+            echo "Prefixing OPCUA_GATEWAY_IDENTIFIER with the device_id to avoid identity clashes" >&2
+            OPCUA_GATEWAY_IDENTIFIER="${DEVICE_ID}:${OPCUA_GATEWAY_IDENTIFIER}"
+            export OPCUA_GATEWAY_IDENTIFIER
+            ;;
+    esac
 fi
 
 cd /app
+echo "Settings"
+echo "  OPCUA_GATEWAY_IDENTIFIER: $OPCUA_GATEWAY_IDENTIFIER" >&2
+echo "  OPCUA_GATEWAY_NAME: $OPCUA_GATEWAY_NAME" >&2
 echo "Starting the opcua-device-gateway..."
 exec /usr/bin/java -Dlogging.config=file:./logging.xml -Dspring.profiles.active=default,tenant -jar opcua-device-gateway.jar -Dspring.config.location=file:./application-tenant.yaml "$@"
