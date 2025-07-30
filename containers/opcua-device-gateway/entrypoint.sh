f1b9f0622401a2fb2408ff25435aa54cfd61c3eb
Marco Stoffel <marco.stoffel@cumulocity.com>
2025-07-30 13:59:11 +0200

    Consolidate gateway var naming

--- containers/opcua-device-gateway/entrypoint.sh
+++ containers/opcua-device-gateway/entrypoint.sh
@@ -17,22 +17,21 @@ if command -V tedge >/dev/null 2>&1; then
 
     # Add a prefix to the OPCUA identifier to ensure its external identity is unique
     # to allow deploying multiple versions in the fleet without having to define unique names all the time
-    case "$OPCUA_GATEWAY_IDENTIFIER" in
+    case "$GATEWAY_IDENTIFIER" in
         *"$GATEWAY_THINEDGE_DEVICEID"*)
             echo "Gateway identifier already includes the device_id" >&2
             ;;
         *)
             echo "Prefixing OPCUA_GATEWAY_IDENTIFIER with the device_id to avoid identity clashes" >&2
-            export GATEWAY_IDENTIFIER="${GATEWAY_THINEDGE_DEVICEID}:${OPCUA_GATEWAY_IDENTIFIER}"
-            export GATEWAY_NAME="${OPCUA_GATEWAY_NAME}"
+            export GATEWAY_IDENTIFIER="${GATEWAY_THINEDGE_DEVICEID}:${GATEWAY_IDENTIFIER}"
             ;;
     esac
 fi
 
 cd /app
 echo "Settings"
-echo "  OPCUA_GATEWAY_IDENTIFIER: $OPCUA_GATEWAY_IDENTIFIER" >&2
-echo "  OPCUA_GATEWAY_NAME: $OPCUA_GATEWAY_NAME" >&2
+echo "  GATEWAY_IDENTIFIER: $OPCUA_GATEWAY_IDENTIFIER" >&2
+echo "  GATEWAY_NAME: $GATEWAY_NAME" >&2
 echo "Starting the opcua-device-gateway..."
 # shellcheck disable=SC2086
 exec /usr/bin/java $JAVA_OPTS -Dlogging.config=file:./logging.xml -Dspring.profiles.active=default -jar opcua-device-gateway.jar  "$@"
