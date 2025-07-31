4d5345819aaa79636e4a410fe2680209bbcc38ea
Marco Stoffel <marco.stoffel@cumulocity.com>
2025-07-31 08:40:16 +0200

    Check for thinEdge mode enabled

--- containers/opcua-device-gateway/entrypoint.sh
+++ containers/opcua-device-gateway/entrypoint.sh
@@ -3,15 +3,17 @@ set -e
 
 # Try loading any values, which aren't already set, using tedge cli
 if [ "$GATEWAY_THINEDGE_ENABLED" = true ]; then
-    echo "Loading values from tedge..."
+    echo "ThinEdge mode enabled, trying to load values from tedge..." >&2
     if command -V tedge >/dev/null 2>&1; then
         if [ -z "$GATEWAY_THINEDGE_DEVICEID" ]; then
+            echo "Fetching GATEWAY_THINEDGE_DEVICEID using tedge command tedge config get device.id" >&2
             GATEWAY_THINEDGE_DEVICEID="$(tedge config get device.id 2>/dev/null ||:)"
             echo "Using value from tedge: GATEWAY_THINEDGE_DEVICEID=$GATEWAY_THINEDGE_DEVICEID" >&2
             export GATEWAY_THINEDGE_DEVICEID
         fi
 
         if [ -z "$C8Y_BASEURL" ] || [ "$C8Y_BASEURL" = "https://" ] || [ "$C8Y_BASEURL" = "http://" ]; then
+            echo "Fetching C8Y_BASEURL using tedge command tedge config get c8y.url" >&2
             C8Y_BASEURL="https://$(tedge config get c8y.url 2>/dev/null ||:)"
             echo "Using value from tedge: C8Y_BASEURL=$C8Y_BASEURL" >&2
             export C8Y_BASEURL
@@ -24,7 +26,7 @@ if [ "$GATEWAY_THINEDGE_ENABLED" = true ]; then
                 echo "Gateway identifier already includes the device_id" >&2
                 ;;
             *)
-                echo "Prefixing OPCUA_GATEWAY_IDENTIFIER with the device_id to avoid identity clashes" >&2
+                echo "Prefixing GATEWAY_IDENTIFIER with the GATEWAY_THINEDGE_DEVICEID to avoid identity clashes" >&2
                 export GATEWAY_IDENTIFIER="${GATEWAY_THINEDGE_DEVICEID}:${GATEWAY_IDENTIFIER}"
                 ;;
         esac
@@ -33,8 +35,9 @@ fi
 
 cd /app
 echo "Settings"
-echo "  GATEWAY_IDENTIFIER: $GATEWAY_IDENTIFIER" >&2
-echo "  GATEWAY_NAME: $GATEWAY_NAME" >&2
-echo "Starting the opcua-device-gateway..."
+echo "GATEWAY_IDENTIFIER: $GATEWAY_IDENTIFIER" >&2
+echo "GATEWAY_NAME: $GATEWAY_NAME" >&2
+echo "C8Y_BASEURL: $C8Y_BASEURL" >&2
+echo "Starting the opcua-device-gateway..." >&2
 # shellcheck disable=SC2086
-exec /usr/bin/java $JAVA_OPTS -Dlogging.config=file:./logging.xml -Dspring.profiles.active=default -jar opcua-device-gateway.jar  "$@"
+exec /usr/bin/java $JAVA_OPTS -Dlogging.config=file:./logging.xml -Dspring.profiles.active=default -jar opcua-device-gateway.jar "$@"
