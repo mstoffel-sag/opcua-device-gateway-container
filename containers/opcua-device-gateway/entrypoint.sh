50b03d231f0f500d244619f0433b51187f6e360b
Marco Stoffel <marco.stoffel@cumulocity.com>
2026-03-13 13:49:18 +0100

    Remove automatic BASEURL evaluation

--- containers/opcua-device-gateway/entrypoint.sh
+++ containers/opcua-device-gateway/entrypoint.sh
@@ -6,13 +6,19 @@ if [ "$GATEWAY_THINEDGE_ENABLED" = true ]; then
     echo "ThinEdge mode enabled, trying to get device_id from c8y over proxy..."
 
         if [ -z "$C8Y_BASEURL" ]; then
-            echo "No C8Y_BASEURL set, setting to localhost thinEdge Proxy only on native"
-            export C8Y_BASEURL=http://127.0.0.1:8001/c8y
+            echo "ERROR: No C8Y_BASEURL set, please define it as an environment variable like export C8Y_BASEURL=http://tedge-container:8001/c8y"
+            exit 1
         fi
 
         if [ -z "$GATEWAY_THINEDGE_DEVICEID" ]; then
-            echo "Fetching GATEWAY_THINEDGE_DEVICEID using device user for cumulocity to get device.id http://127.0.0.1:8001/c8y/user/currentUser" 
-            GATEWAY_THINEDGE_DEVICEID="$(curl $C8Y_BASEURL/user/currentUser | jq -r '.id' | cut -d'_' -f2)"
+            echo "Fetching GATEWAY_THINEDGE_DEVICEID using device user for cumulocity to get device.id $C8Y_BASEURL/user/currentUser" 
+            GATEWAY_THINEDGE_DEVICEID="$(curl -sS $C8Y_BASEURL/user/currentUser | jq -r '.id' | cut -d'_' -f2)"
+            
+            if [ -z "$GATEWAY_THINEDGE_DEVICEID" ]; then
+                echo "ERROR: Failed to fetch GATEWAY_THINEDGE_DEVICEID from $C8Y_BASEURL/user/currentUser"
+                exit 1
+            fi
+            
             echo "Got GATEWAY_THINEDGE_DEVICEID=$GATEWAY_THINEDGE_DEVICEID" 
             export GATEWAY_THINEDGE_DEVICEID
         fi
@@ -32,7 +38,7 @@ if [ "$GATEWAY_THINEDGE_ENABLED" = true ]; then
 fi
 
 cd /app
-echo "Settings"
+echo "Settings:"
 echo "GATEWAY_IDENTIFIER: $GATEWAY_IDENTIFIER"
 echo "GATEWAY_NAME: $GATEWAY_NAME"
 echo "C8Y_BASEURL: $C8Y_BASEURL"
